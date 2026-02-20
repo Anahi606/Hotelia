f108b065a49e02133331c757b72f4cab041b1434
Anahi Quezada <aniequezada606@gmail.com>
2026-02-20 13:40:52 -0500

    Carga inicial del proyecto de Unity

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput; 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
